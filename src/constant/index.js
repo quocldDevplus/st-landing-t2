6436f1b6246ed16abd6383281e146a4a4830ff6c
quocle <quoc.le_dp04@devplus.edu.vn>
2022-11-29 17:40:26 +0700

    feature: create folders structure

---
+++ src/constant/index.js
@@ -0,0 +1,16 @@
+export const menuItems = [
+    { id: 1, content: 'Home' },
+    {
+        id: 2,
+        content: 'What we do',
+        childrens: [
+            { id: 1, content: 'St Software' },
+            { id: 2, content: 'St Digital' },
+            { id: 3, content: 'St Icubation' },
+        ],
+    },
+    { id: 3, content: 'Who we are' },
+    { id: 4, content: 'Portfolio' },
+    { id: 5, content: 'Join us' },
+    { id: 6, content: `Let's talk` },
+];
