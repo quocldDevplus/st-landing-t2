33395952acd245f71bf3d5d58c0834b44b93254c
danhphamcongbang <bang.danh_dp04@devplus.edu.vn>
2022-12-12 16:18:57 +0700

    feature: fix bug scss

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,10 +4,11 @@ import WhoWeAre from '~/pages/WhoWeAre';
 import Portfolio from "~/pages/Portfolio";
 import stSoftWare from "~/pages/stSoftWare/StSoftWare";
 import JoinUs from "~/pages/JoinUs/JoinUs";
+import StDigitalPage from "~/pages/StDigital/StDigital";
 const publicRoutes = [
     { path: '/', component: Home },
     { path: '/st-software', component: stSoftWare },
-    { path: '/st-digital', component: WhoWeAre },
+    { path: '/st-digital', component: StDigitalPage },
     { path: '/st-incubation', component: WhoWeAre },
     { path: '/who-we-are', component: WhoWeAre },
     { path: '/portfolio', component: Portfolio },
