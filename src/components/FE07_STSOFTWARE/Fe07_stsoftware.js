b47c88ec07f0a9a06c9fce774ebf0437adfd0b6d
danhphamcongbang <bang.danh_dp04@devplus.edu.vn>
2022-12-13 14:43:31 +0700

    update API portfolio

--- src/components/FE07_STSOFTWARE/Fe07_stsoftware.js
+++ src/components/FE07_STSOFTWARE/Fe07_stsoftware.js
@@ -6,83 +6,6 @@ import Fe07Header from "~/components/FE07_STSOFTWARE/Fe07Header/Fe07Header";
 import {getAllDataFE02, getAllDataFE07Div1, getAllDataFE07Div2} from "~/service/Apiservice";
 
 const Fe07_stsoftware = (props) => {
-    // const [listFe07, setListFe07] = useState([
-    //     {
-    //         id: "fe07-1",
-    //         image: "https://stunited.vn/wp-content/uploads/2019/09/Capture.png"
-    //         ,
-    //         teamCaption: "eHealth",
-    //         teamDesigner: "#ASP.NET #Android #iOS",
-    //         teamCaptionHide: "A health data management application for " +
-    //             "the personal healthcare in coordination with employers, pharmacies and medical examination providers."
-    //     },
-    //     {
-    //         id: "fe07-2", image: "https://stunited.vn/wp-content/uploads/2019/09/image-logo-1.png"
-    //         , teamCaption: "Havuta", teamDesigner: "#Blockchain #NodeJS #ReactJS #React Native",
-    //         teamCaptionHide: "\n" +
-    //             "                            Havuta brings smartphone and blockchain-based solutions to the data collection challenges facing the development sector. Creating a blockchain-based feedback loop benefiting NGOs, engaging their beneficiaries, and promoting transparency. "
-    //     },
-    //     {
-    //         id: "fe07-3", image: "https://stunited.vn/wp-content/uploads/2019/09/figure01.jpg"
-    //         , teamCaption: "E-ASPRO", teamDesigner: "#Javascript #HTML #CSS",
-    //         teamCaptionHide: "E-ASPRO is a cloudtype fulfillment mail order solution that enables you to efficiently manage mail order operations such as site management," +
-    //             " orders, shipping, inventory, and shipping inquiries from one system"
-    //     },
-    //     {
-    //         id: "fe07-4",
-    //         image: "https://stunited.vn/wp-content/uploads/2019/09/2e-jycg6_400x400.jpg"
-    //         ,
-    //         teamCaption: "Vision Express",
-    //         teamDesigner: "#Chatbot #Javascript #HTML #CSS",
-    //         teamCaptionHide: "Chatbot to communicate with users, reply questions automatically."
-    //     },
-    //
-    // ])
-    // const [listFe07, setListFe07] = useState([])
-    // const [listContextFe07D1, setListContextFe07D1] = useState([
-    //     {
-    //         id: "fe07-1", icon: <BsFillFlagFill/>, title: "Web App Development",
-    //         content: "We create beautifully designed web apps precisely tailored t" +
-    //             "o your situationWe deliver web systems of any size and complexity — from budding startu" +
-    //             "p products to hi-end enterprise solutions.",
-    //         link: "see more"
-    //     },
-    //     {
-    //         id: "fe07-2", icon: <BsFillFlagFill/>, title: "Mobile App Development",
-    //         content: "ST provides custom mobile application development across different\n" +
-    //             "platforms such as iOS and Android for consumer-facing and enterprise\n" +
-    //             "environment.",
-    //         link: "see more"
-    //     },
-    //     {
-    //         id: "fe07-3", icon: <BsFillFlagFill/>, title: "Custom Software Development",
-    //         content: "T provides high quality, cost-effective and reliable software development services match your specific needs, budget and timeframe." +
-    //             " We love to competes with other firms on the grounds of quality, creativity, n’ dedication",
-    //         link: "see more"
-    //     }
-    // ])
-    // const [listContextFe07D2, setListContextFe07D2] = useState([
-    //     {
-    //         id: "fe07-4", icon: <FaUsers/>, title: "Dedicated Team",
-    //         content: "Dedicated Team model of cooperation provides high-quality software development services for our clients. " +
-    //             "Our Dedicated Teams offer clients a simple, affordable answer to build-up of any software development project." +
-    //             " Team sizes are adjusted to accommodate changing requirements and other needs.",
-    //         link: "see more"
-    //     },
-    //     {
-    //         id: "fe07-5", icon: <BsFillFlagFill/>, title: "Technology Consulting",
-    //         content: "Whether you are optimizing costs, rolling out new IT initiatives, or identifying new growth opportunities, ST offers" +
-    //             " technology and software consulting services to help you respond to change and transform your business.",
-    //         link: "see more"
-    //     },
-    //     {
-    //         id: "fe07-6", icon: <BsFillFlagFill/>, title: "AI/Machine Learning",
-    //         content: "Using the power of machine learning tools and technologies, our AI experts and development team offer " +
-    //             "comprehensive and cutting-edge solutions to your current or potential business needs.",
-    //         link: "Read more"
-    //     }
-    //
-    // ])
     const [listContextFe07D1, setListContextFe07D1] = useState([])
     const [listContextFe07D2, setListContextFe07D2] = useState([])
     const [listFe07,setListFe07]=useState([])
@@ -139,7 +62,7 @@ const Fe07_stsoftware = (props) => {
                                                     <Col className="fe07-context__wpb--block--content">
                                                         <h3>{item.title}</h3>
                                                         <p>{item.content}</p>
-                                                        <a href="#">{item.link}</a>
+                                                        <a href="">{item.link}</a>
                                                     </Col>
                                                 </Col>
                                             </Row>
