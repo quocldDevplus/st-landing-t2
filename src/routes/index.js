108d2037a5566b5fc3a88e0e8edff4e180f9c37a
lamde <fatandfeed@gmail.com>
2022-12-16 17:11:14 +0700

    feture: whoweare and lets talk building

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,10 +1,11 @@
 //Layouts
 import Home from '~/pages/Home';
 import WhoWeAre from '~/pages/WhoWeAre';
-import Portfolio from "~/pages/Portfolio";
-import stSoftWare from "~/pages/stSoftWare/StSoftWare";
-import JoinUs from "~/pages/JoinUs/JoinUs";
-import StDigitalPage from "~/pages/StDigital/StDigital";
+import Portfolio from '~/pages/Portfolio';
+import stSoftWare from '~/pages/stSoftWare/StSoftWare';
+import JoinUs from '~/pages/JoinUs/JoinUs';
+import StDigitalPage from '~/pages/StDigital/StDigital';
+import Pageletstalk from '~/pages/LetsTalk/Pageletstalk';
 const publicRoutes = [
     { path: '/', component: Home },
     { path: '/st-software', component: stSoftWare },
@@ -13,7 +14,7 @@ const publicRoutes = [
     { path: '/who-we-are', component: WhoWeAre },
     { path: '/portfolio', component: Portfolio },
     { path: '/join-us', component: JoinUs },
-    { path: '/lets-talk', component: WhoWeAre },
+    { path: '/lets-talk', component: Pageletstalk },
 ];
 
 const privateRoutes = [];
